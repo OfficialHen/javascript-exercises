aa3dc7eb99eb041bf68fc94b67c7fc5e29367c23
Henry Zheng <nothenryzheng@gmail.com>
2025-07-21 16:39:31 -0400

    Completed 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, amount) {
+    if (amount < 0) return 'ERROR'
+    let string = ''
+    for (let i = 0; i < amount; i++) {
+        string += phrase
+    }
+    return string
 };
 
 // Do not edit below this line
