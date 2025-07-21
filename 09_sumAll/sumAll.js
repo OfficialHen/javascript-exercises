997582ba58d4029d4bbb945d707fcb713edaa1b8
Henry Zheng <nothenryzheng@gmail.com>
2025-07-21 18:13:17 -0400

    Completed 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    function isValid(x) {
+        return typeof x === 'number' && Number.isInteger(x) && x >= 0
+    }
+    if (!isValid(a) || !isValid(b)) return 'ERROR';
+    
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    let sum = 0;
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
