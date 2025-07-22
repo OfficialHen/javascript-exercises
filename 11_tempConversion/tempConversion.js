03b10c3c3e0ee5cc7f1f17f5977c66661bc92071
Henry Zheng <nothenryzheng@gmail.com>
2025-07-22 01:03:07 -0400

    Completed 10_leapYears and 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function roundTenth (num) {
+  return Math.round(num * 10) / 10
+}
+
+const convertToCelsius = function(fahrenheit) {
+  return roundTenth((fahrenheit - 32) * (5/9))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return roundTenth(celsius * (9/5) + 32)
 };
 
 // Do not edit below this line
