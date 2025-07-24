8765623c8f38c1c4231fa2dd2e9816ee7fcd3ce1
Henry Zheng <nothenryzheng@gmail.com>
2025-07-24 17:39:40 -0400

    Completed 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+	return num.reduce((prev, next) => prev+next,0)
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  return num.reduce((prev, next) => prev*next)
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x,y)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	if (num !== 0) {
+    for (let i=1; i <= num; i++) {
+      result *=i
+    }
+  }
+  
+  return result
 };
 
 // Do not edit below this line
