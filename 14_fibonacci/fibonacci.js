e47cfb3518e110c19c5ac1a068cc3483d2efff57
Henry Zheng <nothenryzheng@gmail.com>
2025-07-24 18:26:09 -0400

    Completed 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num)
+    if (num===0) return 0
+    if (num===1) return 1
+    if (num<0) return 'OOPS'
 
+    let a = 0
+    let b = 1
+    for (let i=2; i<=num; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b
 };
 
 // Do not edit below this line
