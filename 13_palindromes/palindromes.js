b1a6a0e4403708b4aacab03acd8fc2fc8f88efe3
Henry Zheng <nothenryzheng@gmail.com>
2025-07-24 18:04:17 -0400

    Completed 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arr = string
+                .toLowerCase()
+                .split('')
+                .filter(char => /[a-z0-9]/.test(char))
 
+    let reverseArr = [...arr].reverse()
+
+    return arr.join('') === reverseArr.join('');
 };
 
 // Do not edit below this line
