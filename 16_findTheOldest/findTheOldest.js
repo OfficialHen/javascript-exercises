7774f7f9305d946ffe96b07a6fced58cb0fefb4b
Henry Zheng <nothenryzheng@gmail.com>
2025-07-24 20:29:58 -0400

    Completed 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    const currentYear = new Date().getFullYear()
 
+    return obj.reduce((oldest, person) => {
+        const age = (person.yearOfDeath || currentYear) - person.yearOfBirth
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth
+
+        return age > oldestAge ? person : oldest
+    })
 };
 
 // Do not edit below this line
